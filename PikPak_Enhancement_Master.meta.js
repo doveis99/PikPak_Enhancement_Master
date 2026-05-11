d67478c5ed011eb322cb2e355e64a00d252eb510
digbug82 <weiinchina2001@gmail.com>
2026-05-11 23:48:46 +0800

    Create PikPak_Enhancement_Master.meta.js

---
+++ PikPak_Enhancement_Master.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name               PikPak 增强大师
+// @name:zh-CN         PikPak 增强大师
+// @name:zh-TW         PikPak 增強大師
+// @name:en            PikPak Enhancement Master
+// @name:ko            PikPak Enhancement Master
+// @name:ja            PikPak Enhancement Master
+// @name:id            PikPak Enhancement Master
+// @name:ms            PikPak Enhancement Master
+// @namespace          https://github.com/digbug82/
+// @version            2.4.0
+// @author             digbug82
+// @license            CC-BY-NC-SA-4.0
+// @match              https://mypikpak.com/drive/*
+// @match              https://app.mypikpak.com/*
+// @match              https://drive.mypikpak.com/*
+// @downloadURL        https://raw.githubusercontent.com/digbug82/PikPak_Enhancement_Master/main/PikPak_Enhancement_Master.user.js
+// @updateURL          https://raw.githubusercontent.com/digbug82/PikPak_Enhancement_Master/main/PikPak_Enhancement_Master.meta.js
+// ==/UserScript==
